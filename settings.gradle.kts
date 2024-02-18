7310e6cfe6cf786e8ff28bd10a22d3a59bce715b
Assel <132613032+Soulrich08@users.noreply.github.com>
2024-02-19 01:58:32 +0600

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MSWorksheet-4(Lab-2)"
+include(":app")
+ 
